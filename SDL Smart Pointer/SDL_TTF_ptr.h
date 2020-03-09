91c6703419f89d336a902972261f3d56c5f0e038
remaa <kamboz0@ukr.net>
2020-03-09 02:07:56 +0200

    Added support for shared_ptr

--- SDL Smart Pointer/SDL_TTF_ptr.h
+++ SDL Smart Pointer/SDL_TTF_ptr.h
@@ -17,4 +17,10 @@ using namespace SDL_TTF_Deleter;
 
 using SDL_TTF_Font_ptr = std::unique_ptr<TTF_Font, SDL_TTF_Font_Deleter>;
 
+template <class... Args>
+std::shared_ptr<TTF_Font> make_TTF_Font_s(Args&&... args) {
+	return std::shared_ptr<TTF_Font>(std::forward<Args>(args)...,
+		SDL_TTF_Font_Deleter{});
+}
+
 #endif // !SDL_TTF_PTR_H
--- SDL Smart Pointer/SDL_ptr.h
+++ SDL Smart Pointer/SDL_ptr.h
@@ -82,7 +82,6 @@ namespace SDL_Deleter
 using namespace SDL_Deleter;
 
 
-
 using SDL_Surface_ptr = std::unique_ptr<SDL_Surface, SDL_Surface_Deleter>;
 
 using SDL_Texture_ptr = std::unique_ptr<SDL_Texture, SDL_Texture_Deleter>;
@@ -106,6 +105,79 @@ using SDL_Palette_ptr = std::unique_ptr<SDL_Palette, SDL_Palette_Deleter>;
 using SDL_RWops_ptr = std::unique_ptr<SDL_RWops, SDL_RWops_Deleter>;
 
 using SDL_Uint8_WAV_ptr = std::unique_ptr<Uint8, SDL_Unit8_WAV_Deleter>;
+
+
+template <class... Args>
+std::shared_ptr<SDL_Surface> make_SDL_Surface_s(Args&&... args) {
+	return std::shared_ptr<SDL_Surface>(std::forward<Args>(args)...,
+		SDL_Surface_Deleter{});
+}
+
+template <class... Args>
+std::shared_ptr<SDL_Texture> make_SDL_Texture_s(Args&&... args) {
+	return std::shared_ptr<SDL_Texture>(std::forward<Args>(args)...,
+		SDL_Texture_Deleter{});
+}
+
+template <class... Args>
+std::shared_ptr<SDL_Window> make_SDL_Window_s(Args&&... args) {
+	return std::shared_ptr<SDL_Window>(std::forward<Args>(args)...,
+		SDL_Window_Deleter{});
+}
+
+template <class... Args>
+std::shared_ptr<SDL_Renderer> make_SDL_Renderer_s(Args&&... args) {
+	return std::shared_ptr<SDL_Renderer>(std::forward<Args>(args)...,
+		SDL_Renderer_Deleter{});
+}
+
+template <class... Args>
+std::shared_ptr<SDL_mutex> make_SDL_mutex_s(Args&&... args) {
+	return std::shared_ptr<SDL_mutex>(std::forward<Args>(args)...,
+		SDL_mutex_Deleter{});
+}
+
+template <class... Args>
+std::shared_ptr<SDL_cond> make_SDL_cond_s(Args&&... args) {
+	return std::shared_ptr<SDL_cond>(std::forward<Args>(args)...,
+		SDL_cond_Deleter{});
+}
+
+template <class... Args>
+std::shared_ptr<SDL_sem> make_SDL_sem_s(Args&&... args) {
+	return std::shared_ptr<SDL_sem>(std::forward<Args>(args)...,
+		SDL_sem_Deleter{});
+}
+
+template <class... Args>
+std::shared_ptr<SDL_Cursor> make_SDL_Cursor_s(Args&&... args) {
+	return std::shared_ptr<SDL_Cursor>(std::forward<Args>(args)...,
+		SDL_Cursor_Deleter{});
+}
+
+template <class... Args>
+std::shared_ptr<SDL_PixelFormat> make_SDL_PixelFormat_s(Args&&... args) {
+	return std::shared_ptr<SDL_PixelFormat>(std::forward<Args>(args)...,
+		SDL_PixelFormat_Deleter{});
+}
+
+template <class... Args>
+std::shared_ptr<SDL_Palette> make_SDL_Palette_s(Args&&... args) {
+	return std::shared_ptr<SDL_Palette>(std::forward<Args>(args)...,
+		SDL_Palette_Deleter{});
+}
+
+template <class... Args>
+std::shared_ptr<SDL_RWops> make_SDL_RWops_s(Args&&... args) {
+	return std::shared_ptr<SDL_RWops>(std::forward<Args>(args)...,
+		SDL_RWops_Deleter{});
+}
+
+template <class... Args>
+std::shared_ptr<Uint8> make_SDL_Uint8_WAV_s(Args&&... args) {
+	return std::shared_ptr<Uint8>(std::forward<Args>(args)...,
+		SDL_Unit8_WAV_Deleter{});
+}
 
 
 #endif // !SDL_PTR_H
